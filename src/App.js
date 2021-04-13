e7abcbab1384b157ab367f289380810b914b377e
riqkml <riqkikamal@outlook.com>
2021-04-13 14:01:41 +0700

    add loading animation

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import LoadingBubble from './LoadingBubble'
+
+export default class App extends Component {
+  render() {
+    return <LoadingBubble />
+  }
+}
+
+const styles = StyleSheet.create({})
