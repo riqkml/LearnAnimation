b50e1f975be6833844b771aca06023b4664a3ccf
riqkml <riqkikamal@outlook.com>
2021-04-13 21:41:16 +0700

    animation random box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 import LoadingBubble from './LoadingBubble'
+import RandomBox from './RandomBox'
 
 export default class App extends Component {
   render() {
-    return <LoadingBubble />
+    return <RandomBox />
   }
 }
 
---
+++ src/RandomBox.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View, Animated, TouchableOpacity } from 'react-native'
+
+export default class RandomBox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            position: new Animated.ValueXY(0, 0)
+        }
+    }
+
+    _handleRandom = () => {
+        Animated.parallel([
+            Animated.spring(this.state.position.x, {
+                toValue: Math.floor(Math.random() * 201) - 100,
+                speed: 1,
+                useNativeDriver: true
+            }),
+            Animated.spring(this.state.position.y, {
+                toValue: Math.floor(Math.random() * 201) - 100,
+                speed: 1,
+                useNativeDriver: true
+            })
+        ]).start()
+    }
+
+
+    render() {
+        return (
+            <View style={{ justifyContent: 'center', flex: 1, alignItems: 'center' }}>
+                <Animated.View style={{ backgroundColor: 'red', width: 100, height: 100, transform: [{ translateX: this.state.position.x }, { translateY: this.state.position.y }] }} >
+                    <TouchableOpacity onPress={() => this._handleRandom()} style={{ width: 100, height: 100 }} />
+                </Animated.View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({})
